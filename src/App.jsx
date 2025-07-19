273bde5d4f014aaed7698bf5753927ed4e9c8572
shayla-develops-webs <shaylamrodgers@gmail.com>
2025-07-19 13:19:14 -0500

    add nav bar component and applied across all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import LandingPage from './pages/LandingPage.jsx'
-import PatientInformation from './pages/PatientInformation.jsx'
-import PatientStatusUpdate from './pages/PatientStatusUpdate.jsx'
-import PatientStatus from './pages/PatientStatus.jsx'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import LandingPage from "./pages/LandingPage.jsx";
+import PatientInformation from "./pages/PatientInformation.jsx";
+import PatientStatusUpdate from "./pages/PatientStatusUpdate.jsx";
+import PatientStatus from "./pages/PatientStatus.jsx";
+import NavBar from "./components/ui/navbar.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
+
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/patient-information" element={<PatientInformation />} />
-        <Route path="/patient-status-update" element={<PatientStatusUpdate />} />
+        <Route
+          path="/patient-status-update"
+          element={<PatientStatusUpdate />}
+        />
         <Route path="/patient-status" element={<PatientStatus />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
