ac1e3cc4969715f41e7482ffa374f7f6e12a3b84
Francisco Cuevas <124302532+fcuevas6@users.noreply.github.com>
2025-07-23 09:05:10 -0400

    Fix Link wrapper to entire button
    Fixed the bug that was mentioned by Conor

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,15 +6,15 @@ export default function LandingPage() {
     <main className="min-h-screen bg-blue-50 flex items-center justify-center px-4 sm:px-6">
       <section className="max-w-xl w-full text-center bg-white rounded-2xl shadow-lg border border-blue-100 p-8 sm:p-10">
         <h1 className="text-4xl sm:text-5xl font-bold text-blue-700 tracking-tight">
-          Peace of mind, one update at a time.
+          Peace of mind,<br/> one update at a time.
         </h1>
         <p className="my-10 text-md sm:text-lg text-slate-600">
           Get real-time updates on your loved one's surgery.
         </p>
        
         <div className="flex justify-around mt-6">
-          <Button size="lg" variant="outline"><Link to="/patient-status">Enter as Guest</Link></Button>
-          <Button size="lg"><Link to=""> Authorized Login</Link></Button>
+          <Link to="/patient-status"><Button size="lg" variant="outline">Enter as Guest</Button></Link>
+          <Link to=""><Button size="lg">Authorized Login</Button></Link>
         </div>
       </section>
     </main>
