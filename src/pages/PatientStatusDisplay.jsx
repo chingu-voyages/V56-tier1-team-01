e3c3c75779e3f2b4a24b48b81368dad9e0c3121f
jp249 <vangipierre18@gmail.com>
2025-07-24 15:16:34 -0400

    Test Merge Dev

---
+++ src/pages/PatientStatusDisplay.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function patientStatusDisplay() {
+  return (
+    <div>
+      <h1>Patient Status Display</h1>
+      <p>This page will show the detailed status of the patient.</p>
+    </div>
+  )
+}
+
+export default patientStatusDisplay
