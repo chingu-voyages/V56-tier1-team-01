6917b8c683afc708a5b6839e093c7409956331db
Andrew Butler <abutler911@gmail.com>
2025-07-27 15:33:03 -0700

    feat: implement responsive nav with ShadCN sheet and a custom md breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import LandingPage from "./pages/LandingPage.jsx";
-import PatientInfoPage from "./pages/PatientInfoPage.jsx";
-import PatientStatusUpdate from "./pages/PatientStatusUpdate.jsx";
-import PatientStatus from "./pages/PatientStatus.jsx";
-import NavBar from "./components/ui/navbar.jsx";
-import { PatientProvider } from "./context/PatientProvider";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import LandingPage from './pages/LandingPage.jsx';
+import PatientInfoPage from './pages/PatientInfoPage.jsx';
+import PatientStatusUpdate from './pages/PatientStatusUpdate.jsx';
+import PatientStatus from './pages/PatientStatus.jsx';
+import NavBar from './components/layout/NavBar.jsx';
+import { PatientProvider } from './context/PatientProvider';
 
 function App() {
   return (
@@ -14,13 +14,13 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/patient-information" element={<PatientInfoPage />} />
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/patient-information' element={<PatientInfoPage />} />
           <Route
-            path="/patient-status-update"
+            path='/patient-status-update'
             element={<PatientStatusUpdate />}
           />
-          <Route path="/patient-status" element={<PatientStatus />} />
+          <Route path='/patient-status' element={<PatientStatus />} />
         </Routes>
       </BrowserRouter>
     </PatientProvider>
