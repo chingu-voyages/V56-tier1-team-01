2dc02bdfb63d997e164a12ae147c87d1d514f9e6
Andrew Butler <abutler911@gmail.com>
2025-08-02 15:21:09 -0600

    Rename NavBar.jsx to Header.jsx and update imports

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import NavBar from '@/components/layout/NavBar';
+import Header from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
 
 const MainLayout = ({ children }) => {
   return (
     <div className='flex flex-col min-h-screen'>
-      <NavBar />
+      <Header />
       <main className='flex-1 px-4 py-6 max-w-5xl w-full mx-auto'>
         {children}
       </main>
