117b132b98fa175613eaae21e2e913422f35d42c
Francisco Cuevas <124302532+fcuevas6@users.noreply.github.com>
2025-08-11 12:46:54 -0400

    add alerts in UI for login errors
    added validation to login form to add alerts under the inputs for required fields. as well as for unauthorized username and password.

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { CircleAlert } from 'lucide-react';
 import { useAuth } from "../context/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
@@ -21,20 +23,35 @@ const credentials = [
 export default function LoginForm() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({});
+    const [isUserFound, setIsUserFound] = useState(null);
+
+    const validateLogin = () => {
+        const newErrors = {}
+        if (!username.trim())
+            newErrors.username = "Username is required";
+        if (!password.trim())
+            newErrors.password = "Password is required";
+        setErrors(newErrors)
+        return Object.keys(newErrors).length === 0
+    };
 
     const { login, isAuthenticated, userAccess, setUserAccess } = useAuth();
     const navigate = useNavigate();
 
     function onLogin(submittedUsername, submittedPassword) {
         const userFound = credentials.find(credential =>
-            credential.username === submittedUsername && credential.password === submittedPassword
+            credential.username === submittedUsername.toLowerCase() && credential.password === submittedPassword
         )
 
         if (!userFound) {
-            alert("Invalid username or password");
+            setIsUserFound(false)
+            setUsername("")
+            setPassword("")
             return
         }
 
+        setIsUserFound(true)
         setUserAccess(userFound.access)
         login()
     }
@@ -42,6 +59,7 @@ export default function LoginForm() {
 
     function handleSubmit(e) {
         e.preventDefault();
+        if (!validateLogin()) return;
         onLogin(username, password)
     }
 
@@ -63,29 +81,47 @@ export default function LoginForm() {
                     the status of an existing patient please enter your 
                     authentication information below. 
                 </p>
-                <form 
+                {/* Display error message if user is not found */}
+                {isUserFound === false && (
+                    <Alert className="grid grid-col-2 content-center max-w-sm m-auto mb-6 bg-red-50" variant="destructive" >
+                        <CircleAlert />
+                        <AlertTitle>
+                            Invalid username or password
+                        </AlertTitle>
+                    </Alert>
+
+                )}
+
+                <form
                     className="max-w-sm m-auto"
                     onSubmit={handleSubmit}
                 >
                     <Label>Username:</Label>
                     <Input
-                        className="mb-6"
+                        className="mb-2"
                         placeholder="Username"
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
-                        required
                         />
+                    {errors.username && (
+                        <p className="text-sm text-red-500">{errors.username}</p>
+                    )}
                     <Label>Password:</Label>
                     <Input
+                        className="mb-2"
                         type="password"
                         placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        required
                     />
+                    {errors.password && (
+                        <p className="text-sm text-red-500">{errors.password}</p>
+                    )}
                     <Button className="mt-8" type="submit">Authorize Login</Button>
                 </form>
+
+
 
                 <p className="text-sm text-gray-500 mt-8">Looking for the status of a loved one?</p>
                 <p className="text-sm text-gray-500">Click <a className="underline" href="/patient-status">here</a> to view the patient status board.</p>
